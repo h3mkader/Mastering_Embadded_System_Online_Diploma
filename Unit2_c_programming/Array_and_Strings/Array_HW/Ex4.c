111c0f922e8f031982245ca4797c58d40a684ae8
h3mkader <h3mkader@gmail.com>
2023-04-28 01:33:36 +0300

    Array homework is uploaded

---
+++ Unit2_c_programming/Array_and_Strings/Array_HW/Ex4.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : Ex4.c
+ Author      : Hadeer Emad
+ Copyright   : Your copyright notice
+ Description :C Program to Insert an element in an Array
+ ============================================================================
+ Enter the element to be inserted : 6
+Enter the location: 2
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int arr[100],n,i,num,location;
+	printf("Enter no of elements : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter the element to be inserted : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&num);
+	printf("Enter the location: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&location);
+
+	for(i=n;i>=location;i--)
+	{
+		arr[i]=arr[i-1];
+	}
+	arr[location-1]=num;
+	for(i=0;i<n+1;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+
+
+	return EXIT_SUCCESS;
+}
